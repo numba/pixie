76f2c836471e7cfc7e8215d1311d82e683bf8479
kc611 <ckaustubhm06@gmail.com>
2024-05-23 11:00:17 +0530

    Added basic conda recipe

---
+++ buildscripts/incremental/build.cmd
@@ -0,0 +1,4 @@
+
+python -m pip install -e .
+
+if %errorlevel% neq 0 exit /b %errorlevel%
---
+++ buildscripts/incremental/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Make sure any error below is reported as such
+set -v -e
+
+python -m pip install -e .
