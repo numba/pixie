beda606d504d7d5010faac8b30a9bb53982f5cf2
esc <esc@users.noreply.github.com>
2024-05-23 15:32:47 +0200

    comment out  path such that no warnings are generated
    As title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,4 +30,4 @@ language = 'English'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
