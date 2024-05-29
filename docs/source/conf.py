00beb0cdaee76c932ee8928bd4720cf0109539a1
esc <esc@users.noreply.github.com>
2024-05-29 08:11:49 +0200

    flake8 all the files
    As title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,5 +34,4 @@ language = 'English'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
-
+# html_static_path = ['_static']
