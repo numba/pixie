4b276540796a1aca60c2e95821695fc35cb607a0
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-07-01 11:18:58 -0500

    Fix sysctlbyname ctypes

--- pixie/targets/bsd_utils.py
+++ pixie/targets/bsd_utils.py
@@ -1,15 +1,24 @@
-from ctypes import CDLL, c_char_p, c_void_p, create_string_buffer, byref, c_uint
+from ctypes import (
+    CDLL,
+    c_char_p,
+    c_void_p,
+    c_size_t,
+    POINTER,
+    create_string_buffer,
+    byref,
+)
 from ctypes.util import find_library
 
 libc = CDLL(find_library("c"))
 
 # Define the sysctlbyname function signature
+# int sysctlbyname(const char *, void *, size_t *, void *, size_t);
 libc.sysctlbyname.argtypes = [
-    c_char_p,  # name
-    c_void_p,  # oldp
-    c_void_p,  # oldlenp
-    c_void_p,  # newp
-    c_void_p,  # newlenp
+    c_char_p,           # name
+    c_void_p,           # oldp
+    POINTER(c_size_t),  # oldlenp
+    c_void_p,           # newp
+    c_size_t,           # newlen
 ]
 
 
@@ -22,11 +31,11 @@ def sysctlbyname(name: bytes) -> bytes:
     """
 
     # Determine the size of the output buffer
-    size = c_uint(0)
-    libc.sysctlbyname(name, None, byref(size), None, byref(c_uint(0)))
+    size = c_size_t(0)
+    libc.sysctlbyname(name, None, byref(size), None, 0)
     # Create a buffer of the determined size
     buf = create_string_buffer(size.value)
     # Call sysctlbyname again, this time passing the buffer
-    libc.sysctlbyname(name, buf, byref(size), None, None)
+    libc.sysctlbyname(name, buf, byref(size), None, 0)
     # Return the buffer content
     return buf.raw
